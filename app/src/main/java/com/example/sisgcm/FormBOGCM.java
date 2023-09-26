04786fdf981eca9f9dd932973029a4749117d1a9
estevam22rj <estevammil@hotmail.com>
2023-09-25 21:58:45 -0300

    terceiro commit

--- app/src/main/java/com/example/sisgcm/FormBOGCM.java
+++ app/src/main/java/com/example/sisgcm/FormBOGCM.java
@@ -3,7 +3,9 @@ package com.example.sisgcm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Button;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.*;
 
 public class FormBOGCM extends AppCompatActivity {
 
@@ -13,7 +15,39 @@ public class FormBOGCM extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form_bogcm);
         getSupportActionBar().hide(); //esconder a barra da tela com o nome do programa
+
+        Spinner spinner = findViewById(R.id.spinnercomo);
+
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this,
+                R.array.itens_spinner,
+                android.R.layout.simple_spinner_item
+
+        );
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+
+                String selectedItem = parentView.getItemAtPosition(position).toString();
+                Toast.makeText(getApplicationContext(), "Selecionado: " + selectedItem, Toast.LENGTH_SHORT).show();
+
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                // Ação a ser executada quando nada é selecionado
+            }
+        });
     }
 
 
-}
+    }
+
+
