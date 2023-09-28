e5d10b4a1885977a7d9c77f756c549b97182204e
estevam22rj <estevammil@hotmail.com>
2023-09-28 19:52:42 -0300

    quarto commit (inserido calendario Show DatePicker)

--- app/src/main/java/com/example/sisgcm/FormBOGCM.java
+++ app/src/main/java/com/example/sisgcm/FormBOGCM.java
@@ -2,12 +2,25 @@ package com.example.sisgcm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Spinner;
 import android.widget.*;
+import android.text.InputType;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+import java.util.Calendar;
+
 
 public class FormBOGCM extends AppCompatActivity {
+
+    DatePickerDialog picker;
+    EditText eText;
+
 
 
     @Override
@@ -16,6 +29,33 @@ public class FormBOGCM extends AppCompatActivity {
         setContentView(R.layout.activity_form_bogcm);
         getSupportActionBar().hide(); //esconder a barra da tela com o nome do programa
 
+// inicio do calendario
+        eText=(EditText) findViewById(R.id.edit_data);
+        eText.setInputType(InputType.TYPE_NULL);
+        eText.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+                final Calendar cldr = Calendar.getInstance();
+                int day = cldr.get(Calendar.DAY_OF_MONTH);
+                int month = cldr.get(Calendar.MONTH);
+                int year = cldr.get(Calendar.YEAR);
+                // date picker dialog
+                picker = new DatePickerDialog(FormBOGCM.this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                                eText.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
+                            }
+                        }, year, month, day);
+                picker.show();
+            }
+        });
+//fim do calendario
+
+
+
+//inicio da spinner como foi solicitado
         Spinner spinner = findViewById(R.id.spinnercomo);
 
 
@@ -30,13 +70,17 @@ public class FormBOGCM extends AppCompatActivity {
         spinner.setAdapter(adapter);
 
 
+
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
 
                 String selectedItem = parentView.getItemAtPosition(position).toString();
                 Toast.makeText(getApplicationContext(), "Selecionado: " + selectedItem, Toast.LENGTH_SHORT).show();
-
+// Defina a cor do item selecionado para preto
+                if (selectedItemView != null && selectedItemView instanceof TextView) {
+                    ((TextView) selectedItemView).setTextColor(Color.BLACK);
+                }
 
             }
 
@@ -44,7 +88,7 @@ public class FormBOGCM extends AppCompatActivity {
             public void onNothingSelected(AdapterView<?> parentView) {
                 // Ação a ser executada quando nada é selecionado
             }
-        });
+        }); //final da spinner como foi solicitado
     }
 
 
